ceb5c97cad1866673b90d1ef1c1b1af310719e28
y0uf0ol <uwe@wtsec.de>
2021-04-28 14:21:42 +0200

    removed nmap and changed everything to powershell only

--- NCT/#nmap network scan track changes.ps1
+++ NCT/#nmap network scan track changes.ps1
@@ -1,18 +1,95 @@
 #nmap network scan track changes
 
 ##-------------------VARIABLES-------------------------------
-$oldscan="oLDSCAN.xml"
-$newscan="NEWSCAN.xml"
-$subnet="192.168.0.0/24"
-$outputfile="Diff.txt"
+$FileOut = ".\Scan.csv"
+$FileOutold = ".\ScanOLD.csv"
+$outputfile = ".\ScanDiff.csv"
+## Ping subnet
+$Subnet = "192.168.50."
+$fromaddress = "XXX@yyy.com" 
+$toaddress = "XXX@yyy.com" 
+$DIFFSubject = "Diff in Network found" 
+$OKSubject = "Everything OK" 
+$body = "Diff Found"
+$attachment = $outputfile 
+$smtpserver = "xxxx" 
 
-Remove-Item $oldscan
-Rename-Item $newscan $oldscan
-nmap.exe -sn -oX $newscan $subnet  
 
-Compare-Object -ReferenceObject (Get-Content $newscan) -DifferenceObject (Get-Content $oldscan) > $outputfile
+
+if(Test-Path $FileOut -PathType Leaf){
+    Remove-Item $FileOutold
+    Rename-Item $FileOut $FileOutold
+
+}else {
+    Write-Host 'Initial Scan is missing'
+    Write-Host 'Start initial Scan now'
+
+    #Scan
+    1..254|ForEach-Object{
+        Start-Process -WindowStyle Hidden ping.exe -Argumentlist "-n 1 -l 0 -f -i 2 -w 1 -4 $SubNet$_"
+    }
+    $Computers =(arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +',','|
+      ConvertFrom-Csv -Header Computername,IPv4,MAC,x,Vendor|
+                       Select-Object Computername,IPv4,MAC
+    ForEach ($Computer in $Computers){
+      nslookup $Computer.IPv4|Select-String -Pattern "^Name:\s+([^\.]+).*$"|
+        ForEach-Object{
+          $Computer.Computername = $_.Matches.Groups[1].Value
+        }
+    }
+    $Computers
+    $Computers | Export-Csv $FileOut -NotypeInformation
+    exit
+}
+
+#Scan
+1..254|ForEach-Object{
+    Start-Process -WindowStyle Hidden ping.exe -Argumentlist "-n 1 -l 0 -f -i 2 -w 1 -4 $SubNet$_"
+}
+$Computers =(arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +',','|
+  ConvertFrom-Csv -Header Computername,IPv4,MAC,x,Vendor|
+                   Select-Object Computername,IPv4,MAC
+ForEach ($Computer in $Computers){
+  nslookup $Computer.IPv4|Select-String -Pattern "^Name:\s+([^\.]+).*$"|
+    ForEach-Object{
+      $Computer.Computername = $_.Matches.Groups[1].Value
+    }
+}
+$Computers
+$Computers | Export-Csv $FileOut -NotypeInformation
+
+
+
+#check Diff
+Compare-Object -ReferenceObject (Get-Content $FileOut) -DifferenceObject (Get-Content $FileOutold) > $outputfile
 
 
 ##----------------------------Relay-----------------------
 
+if((Get-Content $outputfile) -eq $Null){
+    #OK Message
+    Write-Host 'Everything is fine'
+    $message = new-object System.Net.Mail.MailMessage 
+    $message.From = $fromaddress 
+    $message.To.Add($toaddress)
+    $message.IsBodyHtml = $True 
+    $message.Subject = $OKSubject
+    $attach = new-object Net.Mail.Attachment($attachment) 
+    $message.Attachments.Add($attach) 
+    $smtp = new-object Net.Mail.SmtpClient($smtpserver) 
+    $smtp.Send($message)
+    Exit
 
+
+}else {
+    $message = new-object System.Net.Mail.MailMessage 
+    $message.From = $fromaddress 
+    $message.To.Add($toaddress)
+    $message.IsBodyHtml = $True 
+    $message.Subject = $DiffSubject 
+    $attach = new-object Net.Mail.Attachment($attachment) 
+    $message.Attachments.Add($attach) 
+    $message.body = $body 
+    $smtp = new-object Net.Mail.SmtpClient($smtpserver) 
+    $smtp.Send($message)
+}
